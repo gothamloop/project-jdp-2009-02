01114ce67c638bef2d3047d7cc7bb134ad1b40f4
gothamloop <g.otham@webtunnelo.com>
2020-10-24 10:38:27 +0200

    JDP200902-24

--- src/main/java/com/kodilla/ecommercee/repository/GenericEntityRepository.java
+++ src/main/java/com/kodilla/ecommercee/repository/GenericEntityRepository.java
@@ -1,6 +1,5 @@
 package com.kodilla.ecommercee.repository;
 
-import com.kodilla.ecommercee.GenericEntity;
 import com.kodilla.ecommercee.domain.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
